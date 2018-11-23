a34bcde9bc3a6606f446940c368e76a4ff7c9f13
Davidov249 <davidov249@gmail.com>
2018-11-23 08:45:39 -0600

    Actualizacion de errores y limpieza de codigo

--- API_proyecto/routes/users.js
+++ API_proyecto/routes/users.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
-const Sync = require('sync')
 const md5 = require('md5')
 const jwt = require('jsonwebtoken')
 const jwtsimple  = require('jwt-simple')
-const CryptoJS = require('crypto-js').CryptoJS
 const mongoClient = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017'
 const dbName = 'ChatDB'
@@ -13,20 +10,23 @@ const dbName = 'ChatDB'
 /* GET token entrada */
 router.get('/login', function(req, res, next) {
   mongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {
-    if (err) res.status(500).send({status: 500, error: 'al conectar'});
+    if (err) 
+    {
+      res.status(500).send({status: 500, error: 'error al conectar'});
+    }
     const database = client.db(dbName)
     const collection = database.collection('usuarios')
     var encpassword = md5(req.query.password)
     collection.find({usuario: req.query.usuario, password: encpassword}).toArray((err, docs)=>{
-      if (err) res.status(500).send({status: 500});
+      if (err) res.status(500).send({status: 500, message: "error al conectarse con la base de datos"});
       if (docs.length == 0){
-        res.status(401).send({status: 401});
+        res.status(404).send({status: 404, message: "La contraseña o el usuario no son correctos"});
       }else{
         var token = generarToken({
           usuario: req.query.usuario,
           password: req.query.password
         }, req.query.secreto)
-        res.status(200).send({status: 200, token: token});
+        res.status(200).send({status: 200, token: token, message: "Se logró iniciar sesion"});
       }
     })
   })
@@ -36,7 +36,8 @@ router.get('/login', function(req, res, next) {
 router.post('/register', (req, res, next) => {
   mongoClient.connect(url, {useNewUrlParser : true}, (err, client)=>{
     if (err) res.status(500).send({
-        status: 500
+        status: 500,
+        message: "error al conectarse con la base de datos"
     })
     var passcon = md5(req.body.password)
     var json1 = {
@@ -52,10 +53,11 @@ router.post('/register', (req, res, next) => {
     collection.insertOne(json1, err => {
         if (err) {
           res.status(500).send({
-            status: 500
+            status: 500, 
+            message: "error al insertar en la base de datos"
         }).end()}
         else{
-          res.status(201).send({status: 201}).end()
+          res.status(201).send({status: 201, message: "registro exitoso"}).end()
         }
     })
 })
@@ -65,27 +67,28 @@ router.post('/register', (req, res, next) => {
 router.put('/actualizar', (req, res, next) =>{
   jwt.verify(req.body.token, req.body.secreto, (err, data)=>{
     if (err){
-      res.status(500).send({status : 500});
+      res.status(401).send({status : 401, message: "token no valido"});
     }else{
       passcon = md5(req.body.password)
       mongoClient.connect(url, {useNewUrlParser: true}, (err, client)=> {
         if (err) res.status(500).send({
-          status: 500
+          status: 500, 
+          message: "error al conetarse con la base de datos"
         })
         const database = client.db(dbName)
         const collection = database.collection('usuarios')
         collection.find({usuario : req.body.usuario, password : passcon}).toArray((err,docs) => {
-            if(err) res.status(500).send({status : 500});
+            if(err) res.status(500).send({status : 500, message: "error en el proceso de busqueda"});
             if(docs == ""){
-              res.status(404).send({status: 404});
+              res.status(404).send({status: 404, message: "Usuario o contraseña no validos"});
             }else{
               collection.findOneAndUpdate({usuario: req.body.usuario}, {$set : req.body.campo},function(err, client) {
                 if (err) throw err;
                 console.log(client)
                 var token = generarToken({usuario: data.usuario, password: data.password}, req.body.secreto)
                 data = {no : "content"}
-                res.status(200).send({status: 200, token: token})
-            })
+                res.status(200).send({status: 200, message: "campo actualizado exitosamente", token: token})
+              })
             }
         })    
       })
@@ -97,19 +100,20 @@ router.put('/actualizar', (req, res, next) =>{
 router.put('/actualizarPassword', (req, res, next) =>{
   jwt.verify(req.body.token, req.body.secreto, (err, data)=>{
     if (err){
-      res.status(500).send({status : 500});
+      res.status(401).send({status : 401, message: "token no valido"});
     }else{
       passcon = md5(req.body.password)
       mongoClient.connect(url, {useNewUrlParser: true}, (err, client)=> {
         if (err) res.status(500).send({
-          status: 500
+          status: 500,
+          message: "error al conectarse con la base de datos"
         })
         const database = client.db(dbName)
         const collection = database.collection('usuarios')
         collection.find({usuario : req.body.usuario, password : passcon}).toArray((err,docs) => {
-            if(err) res.status(500).send({status : 500});
+            if(err) res.status(500).send({status : 500, message: "error en el proceso de busqueda"});
             if(docs == ""){
-              res.status(404).send({status: 404});
+              res.status(404).send({status: 404, message: "Usuario o contraseña no validos"});
             }else{
               req.body.campo.password = md5(req.body.campo.password)
               collection.findOneAndUpdate({usuario: req.body.usuario}, {$set : req.body.campo},function(err, client) {
@@ -117,7 +121,7 @@ router.put('/actualizarPassword', (req, res, next) =>{
                 console.log(client)
                 var token = generarToken({usuario: data.usuario, password: data.password}, req.body.secreto)
                 data = {no : "content"}
-                res.status(200).send({status: 200, token: token})
+                res.status(200).send({status: 200, message: "contraseña actualizado con exito", token: token})
             })
             }
         })    
@@ -131,23 +135,25 @@ router.delete('/borrar/', (req, res) => {
   var secreto = req.body.jwt
   jwt.verify(req.body.token, secreto, (err, data)=> {
     if (err){
-      res.status(403).send({status: 403});
+      res.status(401).send({status: 401, message: "token no valido"});
     }else{
       data = ""
       mongoClient.connect(url, {useNewUrlParser: true}, (err, client) =>{
-        if(err) res.status(500).send({status: 500});
+        if(err) res.status(500).send({status: 500, message: "error al conectarse con la base de datos"});
         var passcon = md5(req.body.password)
         const database = client.db(dbName)
         const collection = database.collection('usuarios')
         collection.find({"usuario" : req.body.usuario, "password" : passcon}).toArray((err,docs) => {
-            if(err) return next(createError(500))
+            if(err) {
+              res.status(500).send({status: 500, message: "Error en el proceso de busqueda"});
+            }
             if(docs == ""){
                 res.status(404).end();
             }else{
                 collection.deleteOne({"usuario" : req.body.usuario, "password" : passcon}, function(err, client) {
                     if (err) throw err;
                     res.status(200).send(
-                      {status : 200})
+                      {status : 200, message: "usuario borrado exitosamente"})
                 })
             }
         })
